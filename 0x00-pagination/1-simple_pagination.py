73ae8aa56e7b733ca2f15493df102541e8f3d950
AbiollaghJames <abiollajames@protonmail.com>
2023-10-21 09:42:47 +0300

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -10,16 +10,17 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """
-        The function should return a tuple of size
-        two containing a start index and an end index
-        corresponding to the range of indexes to return
-        in a list for those particular pagination parameters.
-        """
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
+    """
+    The function should return a tuple of size
+    two containing a start index and an end index
+    corresponding to the range of indexes to return
+    in a list for those particular pagination parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
 
-        return (start_index, end_index)
+    return (start_index, end_index)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
