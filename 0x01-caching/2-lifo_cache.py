43ae19f66ac766be0eb11e17b115aae78a48e284
AbiollaghJames <abiollajames@protonmail.com>
2023-10-25 06:01:28 +0300

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,5 +12,32 @@ class LIFOCache(BaseCaching):
     BaseCaching and is a caching system
     """
     def __init__(self):
+        """constructor"""
         super().__init__()
         self.keys_in_cache = []
+
+    def put(self, key, item):
+        """
+        LIFO Algorithm
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.keys_in_cache.remove(key)
+                else:
+                    key_to_discard = self.keys_in_cache.pop()
+                    del self.cache_data[key_to_discard]
+                    print(f"DISCARD: {key_to_discard}")
+
+        self.cache_data[key] = item
+        self.keys_in_cache.append(key)
+
+    def get(self, key):
+        """
+        Get item from cache_data dict by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
