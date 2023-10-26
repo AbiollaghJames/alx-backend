760c8bd88ac5fcb2f5fa59779e965d41d1debf55
AbiollaghJames <abiollajames@protonmail.com>
2023-10-26 07:55:11 +0300

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -41,6 +41,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Function get page that returns list
+        """
         assert isinstance(page, int)
         assert isinstance(page_size, int)
         assert page > 0
