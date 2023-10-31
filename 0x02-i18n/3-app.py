3adaf28c53f6564f0af582717a3b80deffcdffdf
AbiollaghJames <abiollajames@protonmail.com>
2023-10-31 16:47:04 +0300

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -24,7 +24,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.Config['LANGUAGES'])
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     """ Home/Index page """
     return render_template('3-index.html')
