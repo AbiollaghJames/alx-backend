0b386a83e1693432387aa6072f02bd93b29ce3ee
AbiollaghJames <abiollajames@protonmail.com>
2023-10-31 16:37:18 +0300

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,7 +21,7 @@ class Config:
 @babel.localeselector
 def get_locale() -> str:
     """determine the best match lang"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.Config['LANGUAGES'])
 
 
 @app.route("/")
