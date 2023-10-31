d5871cf81126dfd2d22af1a6ceeca819ac9e704c
AbiollaghJames <abiollajames@protonmail.com>
2023-10-31 16:29:48 +0300

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Get locale from request
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determine the best match lang"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """ Home/Index page """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
