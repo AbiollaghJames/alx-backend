8c16e13daaae360260f94caf9d5680daa786789d
AbiollaghJames <abiollajames@protonmail.com>
2023-11-28 12:20:25 +0300

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import { redis } from "redis";
+const client = redis.createClient();
+
+client.on("connetc", () => {
+    console.log("Redis client connected to the server");
+});
+client.on("error", (err) => {
+    console.error(`Redis client not connected to the server: ${err}`);
+});
+
+const key = "HolbertonSchools";
+const value = {
+    'Portland': 50,
+    'Seattle': 80,
+    'New York': 20,
+    'Bogota': 20,
+    'Cali': 40,
+    'Paris': 2
+}
+
+for (const [k, v] of Object.entries(value)) {
+    client.hset(key, k, v, (err, data) => 
+    redis.print(`Data: ${data}`));
+}
+
+client.hgetall(key, (err, data) => console.log(data));
