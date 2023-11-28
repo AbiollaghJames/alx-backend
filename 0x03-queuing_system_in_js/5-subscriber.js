bfde95eb73ad9997aafc5c3caa91e8af77451b9c
AbiollaghJames <abiollajames@protonmail.com>
2023-11-28 12:55:36 +0300

    Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,4 +1,4 @@
-const redis = require("redis");
+import { redis } from "redis";
 const client = redis.createClient();
 
 client.on("connect", () => console.log("Redis client connected to the server"));
